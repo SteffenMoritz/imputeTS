47e464cf861bd27a61c9f1750e9949eb416ad990
Steffen Moritz <steffen.moritz10@gmail.com>
2016-06-02 15:42:00 +0200

    fix non-ascii chars

--- R/na.kalman.R
+++ R/na.kalman.R
@@ -63,7 +63,8 @@
 #' usermodel <- arima(x,order = c(1,0,1))$model
 #' na.kalman(x,model = usermodel)
 #' 
-#' @references Hyndman RJ and Khandakar Y (2008). "Automatic time series forecasting: the forecast package for R". Journal of Statistical Software, 26(3).#' @import stats
+#' @references Hyndman RJ and Khandakar Y (2008). "Automatic time series forecasting: the forecast package for R". Journal of Statistical Software, 26(3).
+#' @import stats
 #' @import forecast
 #' @export
 
