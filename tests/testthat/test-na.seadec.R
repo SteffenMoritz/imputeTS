039aa05ed341cacb9090c8cd89894e315328969f
SteffenMoritz <steffen.moritz10@gmail.com>
2018-06-19 15:30:00 +0200

    Testat changes

--- tests/testthat/test-na.seadec.R
+++ tests/testthat/test-na.seadec.R
@@ -18,7 +18,7 @@ test_that("Correct results for all options with a modifed tsAirgap dataset (addi
 
 test_that("Correct results for all options with a modifed tsAirgap dataset (additionalNAs at start)",
           {
-            skip_on_cran()
+            skip_on_travis()
             #Using mean over resulting vector to check correctness
             #In order to avoid writing down the complete resulting vector
             #Using rounded version in order to avoid writing down all decimals
@@ -35,6 +35,7 @@ test_that("Correct results for all options with a modifed tsAirgap dataset (addi
 test_that("Correct results for all options with the tsAirgap dataset",
           {
             skip_on_cran()
+            skip_on_travis()
             #Using mean over resulting vector to check correctness
             #In order to avoid writing down the complete resulting vector
             #Using rounded version in order to avoid writing down all decimals
