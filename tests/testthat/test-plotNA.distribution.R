6ee959a02be0765977743c07f9437d2ae35ff2c8
Sebastian Gatscha <gatscha@trafficon.eu>
2019-07-16 14:44:06 +0200

    add plot tests
    now we're at 100% for the plots ;)

--- tests/testthat/test-plotNA.distribution.R
+++ tests/testthat/test-plotNA.distribution.R
@@ -3,9 +3,7 @@ context("plotNA.distribution")
 test_that("Check that plot is running without error",
           {
 
-            expect_that( is.atomic(plotNA_distribution(tsAirgap)), is_true())
             expect_that( is.recursive(plotNA_distribution(tsAirgap)), is_true())
-            expect_that( is.atomic(plotNA.distribution(tsAirgap)), is_true())
             expect_warning(plotNA.distribution(tsAirgap))
             
             ## input not univariate
