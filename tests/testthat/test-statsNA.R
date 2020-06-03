e39bf32c3edb3ab9df920c98c42d951401c4fbec
SteffenMoritz <steffen.moritz10@gmail.com>
2020-06-03 04:40:32 +0200

    Test update to statsNA update

--- tests/testthat/test-statsNA.R
+++ tests/testthat/test-statsNA.R
@@ -3,16 +3,16 @@ context("statsNA")
 
 test_that("Test that function works and prints output",
           {
-            expect_that( statsNA(tsAirgap, printOnly = T), prints_text())
+            expect_that( statsNA(tsAirgap, print_only = T), prints_text())
           })
 
 
 test_that("Test results of function",
           {
-            expect_that(statsNA(tsAirgap, printOnly = F)$numberNAs, equals(13))
-            expect_that(statsNA(tsAirgap, printOnly = F)$naGapLongest, equals(3))
-            expect_that(statsNA(tsAirgap, printOnly = F)$naGapMostOverallNAs, equals(1))
-            expect_that(statsNA(tsAirgap, printOnly = F)$naGapMostFrequent, equals(1))
-            expect_that(statsNA(tsAirgap, printOnly = F)$lengthTimeSeries, equals(144))
+            expect_that(statsNA(tsAirgap, print_only = F)$numberNAs, equals(13))
+            expect_that(statsNA(tsAirgap, print_only = F)$naGapLongest, equals(3))
+            expect_that(statsNA(tsAirgap, print_only = F)$naGapMostOverallNAs, equals(1))
+            expect_that(statsNA(tsAirgap, print_only = F)$naGapMostFrequent, equals(1))
+            expect_that(statsNA(tsAirgap, print_only = F)$lengthTimeSeries, equals(144))
             
           })
