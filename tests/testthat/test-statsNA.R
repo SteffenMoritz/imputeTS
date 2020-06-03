be9edba06a6802db2d19de0e1a120c52abdc31ce
SteffenMoritz <steffen.moritz10@gmail.com>
2020-06-03 04:54:53 +0200

    statsNA test that update v3

--- tests/testthat/test-statsNA.R
+++ tests/testthat/test-statsNA.R
@@ -14,5 +14,6 @@ test_that("Test results of function",
             expect_that(statsNA(tsAirgap, print_only = F)$most_weighty_na_gap, equals(1))
             expect_that(statsNA(tsAirgap, print_only = F)$most_frequent_na_gap, equals(1))
             expect_that(statsNA(tsAirgap, print_only = F)$length_series, equals(144))
-            
+            expect_that(statsNA(tsAirgap, print_only = F)$number_na_gaps, equals(11))
+            expect_that(statsNA(tsNH4, print_only = F)$average_size_na_gaps, equals(5.696774))
           })
